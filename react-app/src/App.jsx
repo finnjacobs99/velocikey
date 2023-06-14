ff099e5cafb29b0e49d43290e70089a8ba5aecb2
Finn Jacobs <finn.jacobs99@gmail.com>
2023-06-14 00:30:49 -0400

    reimplemented core functionality

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Test from './components/Test';
+
 const App = () => {
   return (
     <div className='bg-neutral-800 h-screen flex flex-col justify-center items-center text-white font-mono'>
--- react-app/src/components/Test.jsx
+++ react-app/src/components/Test.jsx
@@ -1,58 +1,167 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { validInput } from './input';
+import { generate } from 'random-words';
 import './test.css';
 
-const prompt = 'sample test';
+const NUM_WORDS = 200;
+const SECONDS = 60;
+
+const words = generate(25);
 
 const Test = () => {
-  const [input, setInput] = useState('');
-  const caretRef = useRef(null);
+  const [timer, setTimer] = useState(SECONDS);
+  const [isFocused, setIsFocused] = useState(false);
+  const [currentInput, setCurrentInput] = useState('');
+  const [input, setInput] = useState([]);
+  const [prevCorrect, setPrevCorrect] = useState(true);
 
-  useEffect(() => {
-    if (caretRef.current) {
-      caretRef.current.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
-    }
-  }, [input]);
+  // Start timer
+  function start() {
+    let interval = setInterval(() => {
+      setTimer((prev) => {
+        if (!prev) clearInterval(interval);
+        else return prev - 1;
+      });
+    }, 1000);
+  }
 
-  const handleKeyPress = (e) => {
-    if (input.length >= prompt.length && e.key !== 'Backspace') return;
+  function handleFocus() {
+    setIsFocused((prev) => !prev);
+  }
+
+  // Handle user input
+  function handleKeyDown(e) {
     if (!validInput.includes(e.key)) return;
-    setInput((prevInput) => {
-      if (e.key === 'Backspace') return prevInput.slice(0, -1);
-      else return [...prevInput, e.key];
-    });
-  };
 
-  const renderLetter = (letter, index) => {
-    const currentLetter = input[index];
-    const displayLetter = (currentLetter !== ' ' && currentLetter) || letter;
-    const color = !currentLetter
-      ? 'text-neutral-500'
-      : currentLetter === letter
-      ? 'text-white'
-      : 'text-red-500 underline';
+    // Move onto next word
+    if (e.key === ' ') {
+      if (currentInput.length && input.length < words.length) {
+        setInput((prev) => {
+          const newInput = [...prev, currentInput];
+          setPrevCorrect(
+            newInput[newInput.length - 1] === words[newInput.length - 1]
+          );
+          return newInput;
+        });
+        setCurrentInput('');
+      }
+    }
 
+    // Handle backspaces
+    else if (e.key === 'Backspace') {
+      if (currentInput.length) setCurrentInput((prev) => prev.slice(0, -1));
+      else if (input.length && !prevCorrect) {
+        setCurrentInput(input.pop());
+        setPrevCorrect(true);
+      }
+    }
+
+    // Update input
+    else if (currentInput.length < 15) setCurrentInput((prev) => prev + e.key);
+  }
+
+  // Render words to screen
+  function renderWords(word, wIdx) {
     return (
-      <span
-        key={index}
-        className={`${color} whitespace-pre-wrap transition-colors ease-in-out duration-300 relative my-[.2rem]`}
-        ref={index === input.length ? caretRef : null}
-      >
-        {index === input.length && (
-          <span className={`absolute right-[.42rem] caret`}>|</span>
-        )}
-        {displayLetter}
+      <span key={wIdx} className='mx-1'>
+        {word.split('').map((letter, lIdx) => {
+          let color = 'text-neutral-500';
+          let rest;
+
+          // set text colors previous words
+          if (wIdx < input.length) {
+            // correct/incorrect
+            if (input[wIdx][lIdx] === letter) color = 'text-white';
+            else {
+              color = 'text-red-500 underline';
+              if (input[wIdx][lIdx] !== undefined) letter = input[wIdx][lIdx];
+            }
+
+            // extra input
+            if (input[wIdx].length > word.length && lIdx === word.length - 1) {
+              rest = (
+                <span className='text-red-500 underline'>
+                  {input[wIdx].substring(word.length, input[wIdx.lenght])}
+                </span>
+              );
+            }
+          }
+
+          // set text colors for current word
+          else if (wIdx === input.length) {
+            // correct/incorrect
+            if (lIdx < currentInput.length) {
+              if (currentInput[lIdx] === letter) color = 'text-white';
+              else {
+                color = 'text-red-500 underline';
+                letter = currentInput[lIdx];
+              }
+            }
+
+            // extra input
+            if (currentInput.length > word.length && lIdx === word.length - 1) {
+              rest = (
+                <span className='text-red-500 underline'>
+                  {currentInput.substring(word.length, currentInput.length)}
+                </span>
+              );
+            }
+          }
+
+          // Render words
+          return (
+            <span
+              key={lIdx}
+              className={`${color} transition-colors ease-in-out duration-300 relative`}
+            >
+              {renderCaret(word, wIdx, lIdx)}
+              {letter}
+              {rest !== undefined && rest}
+            </span>
+          );
+        })}
       </span>
     );
-  };
+  }
+
+  // Renders caret
+  function renderCaret(word, wIdx, lIdx) {
+    if (
+      isFocused &&
+      wIdx === input.length &&
+      (lIdx === currentInput.length ||
+        (lIdx === word.length - 1 && currentInput.length >= word.length))
+    ) {
+      const leftBound = currentInput.length < word.length ? '' : '-';
+      return (
+        <span
+          className={`caret absolute top-[-.1rem] right-[${leftBound}.42rem]`}
+        >
+          |
+        </span>
+      );
+    }
+    return;
+  }
 
   return (
-    <div
-      className='bg-neutral-900 p-6 text-2xl flex flex-wrap outline-none'
-      onKeyDown={handleKeyPress}
-      tabIndex={0}
-    >
-      {prompt.split('').map(renderLetter)}
+    <div className='flex flex-col items-start w-2/3'>
+      <h1>{timer}</h1>
+      <button onClick={start}>Start</button>
+      <div className='bg-neutral-900 p-6 text-2xl flex flex-wrap outline-none relative'>
+        <div
+          className='absolute inset-1 z-10 flex justify-center items-center outline-none backdrop-blur-md focus:backdrop-blur-none transition ease-in-out duration-300 overflow-hidden'
+          onKeyDown={handleKeyDown}
+          tabIndex={0}
+          onFocus={handleFocus}
+          onBlur={handleFocus}
+        >
+          <span className={`${isFocused && 'opacity-0'} text-neutral-300`}>
+            Click to start
+          </span>
+        </div>
+        {words.map(renderWords)}
+      </div>
     </div>
   );
 };
