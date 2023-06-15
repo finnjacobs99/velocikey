39e5618ee345601247ec5979c49fc5728e49c500
Finn Jacobs <finn.jacobs99@gmail.com>
2023-06-14 22:20:29 -0400

    added tracking/displaying metrics

--- react-app/src/components/Test.jsx
+++ react-app/src/components/Test.jsx
@@ -4,26 +4,119 @@ import { generate } from 'random-words';
 import './test.css';
 
 const Test = () => {
-  const [words, setWords] = useState(generate(25));
+  const [numWords, setNumWords] = useState(25);
+  const [words, setWords] = useState(generate(numWords));
   const [isFocused, setIsFocused] = useState(false);
+  const [timer, setTimer] = useState(0);
   const [currentInput, setCurrentInput] = useState('');
   const [input, setInput] = useState([]);
   const [prevCorrect, setPrevCorrect] = useState(true);
+  const [correct, setCorrect] = useState([]);
+  const [wpm, setWpm] = useState(0);
+  const [numTyped, setNumTyped] = useState(0);
+  const [numTypedCorrect, setNumTypedCorrect] = useState(0);
+  const [accuracy, setAccuracy] = useState(100);
+  const [hasEnded, setHasEnded] = useState(true);
 
-  function init(numWords) {
-    setWords(generate(numWords));
+  // Initialize test
+  function init(_numWords) {
+    setNumWords(_numWords);
+    setWords(generate(_numWords));
     setCurrentInput('');
     setInput([]);
     setPrevCorrect(true);
+    setCorrect([]);
+    setHasEnded(true);
+    setWpm(0);
+    setNumTyped(0);
+    setNumTypedCorrect(0);
+    setAccuracy(100);
+    setTimer(0);
   }
 
+  // Set focus of test
   function handleFocus() {
     setIsFocused((prev) => !prev);
   }
 
+  // Start test
+  function start() {
+    setHasEnded(false);
+  }
+
+  // End Test
+  function end() {
+    setHasEnded(true);
+  }
+
+  // Log metrics at end of test
+  useEffect(() => {
+    if (hasEnded && timer && wpm)
+      console.log(`Time: ${timer}\nWPM: ${wpm}\nAccuracy: ${accuracy}`);
+  }, [wpm, timer, accuracy]);
+
+  // Manage timer
+  useEffect(() => {
+    let interval;
+    if (!hasEnded) {
+      interval = setInterval(() => {
+        setTimer((prev) => {
+          const time = (prev += 0.01);
+          return parseFloat(time.toFixed(2));
+        });
+      }, 10);
+    }
+    return () => clearInterval(interval);
+  }, [hasEnded]);
+
+  // Store correctly tpyed words
+  function updateCorrect() {
+    setCorrect(() => {
+      return input.filter((word, index) => word === words[index]);
+    });
+  }
+
+  // On change of input
+  useEffect(() => {
+    updateCorrect();
+    if (input.length === words.length) end();
+  }, [input]);
+
+  // Calculates WPM
+  function calcWPM() {
+    const numChars = correct.join().length;
+    const _wpm = numChars / 5 / (timer / 60);
+    if (correct.length) setWpm(parseFloat(_wpm.toFixed(2)));
+  }
+
+  // On change of correct
+  useEffect(() => {
+    calcWPM();
+  }, [correct]);
+
+  // Calculate accuracy
+  function calcAccuracy() {
+    const _accuracy = (numTypedCorrect / numTyped) * 100;
+    if (!hasEnded) setAccuracy(parseFloat(_accuracy.toFixed(2)));
+  }
+
+  // On change of current input
+  useEffect(() => {
+    calcAccuracy();
+  }, [currentInput]);
+
   // Handle user input
   function handleKeyDown(e) {
     if (!validInput.includes(e.key)) return;
+
+    // Start condition
+    if (
+      !input.length &&
+      !currentInput.length &&
+      e.key !== 'Backspace' &&
+      e.key !== ' '
+    )
+      start();
 
     // Move onto next word
     if (e.key === ' ') {
@@ -49,13 +142,18 @@ const Test = () => {
     }
 
     // Update input
-    else if (currentInput.length < 15) setCurrentInput((prev) => prev + e.key);
+    else if (currentInput.length < 15) {
+      setNumTyped((prev) => prev + 1);
+      if (!hasEnded && words[input.length][currentInput.length] === e.key)
+        setNumTypedCorrect((prev) => prev + 1);
+      setCurrentInput((prev) => prev + e.key);
+    }
   }
 
   // Render words to screen
   function renderWords(word, wIdx) {
     return (
-      <span key={wIdx} className='mx-1'>
+      <span key={wIdx} className='mx-1 my-[2px]'>
         {word.split('').map((letter, lIdx) => {
           let color = 'text-neutral-500';
           let rest;
@@ -116,7 +214,7 @@ const Test = () => {
     );
   }
 
-  // Renders caret
+  // Render caret
   function renderCaret(word, wIdx, lIdx) {
     if (
       isFocused &&
@@ -134,30 +232,53 @@ const Test = () => {
   }
 
   return (
-    <div className='flex flex-col items-start w-2/3'>
-      {/* <h1>{timer}</h1>
-      <button onClick={start}>Start</button> */}
-      <div className='flex justify-center items-center text-xl text-black font-semibold mx-auto py-4'>
-        <button
-          className='mx-2 bg-neutral-100 py-2 px-5 rounded-lg'
-          onClick={() => init(25)}
-        >
-          25
-        </button>
-        <button
-          className='mx-2 bg-neutral-100 py-2 px-5 rounded-lg'
-          onClick={() => init(50)}
-        >
-          50
-        </button>
-        <button
-          className='mx-2 bg-neutral-100 py-2 px-5 rounded-lg'
-          onClick={() => init(100)}
-        >
-          100
-        </button>
+    // Container
+    <div className='flex flex-col items-between w-2/3'>
+      {/* Dashboard */}
+      <div className='flex justify-between p-6'>
+        {/* Metrics */}
+        <div className='flex flex-col text-2xl font-medium mx-1 w-1/3'>
+          <h1 className='mr-5'>
+            Time: {hasEnded ? '--' : timer.toFixed(2) + ' sec'}
+          </h1>
+          <h1 className='mr-5'>WPM: {hasEnded ? '--' : wpm.toFixed(2)}</h1>
+          <h1 className='mr-5'>
+            Accuracy: {hasEnded ? '--' : accuracy.toFixed(2) + '%'}
+          </h1>
+        </div>
+
+        {/* Buttons */}
+        <div className='flex justify-center items-center text-xl text-black font-semibold'>
+          <button
+            className='mx-2 bg-neutral-100 py-2 px-5 rounded-md'
+            onClick={() => init(25)}
+          >
+            25
+          </button>
+          <button
+            className='mx-2 bg-neutral-100 py-2 px-5 rounded-md'
+            onClick={() => init(50)}
+          >
+            50
+          </button>
+          <button
+            className='mx-2 bg-neutral-100 py-2 px-5 rounded-md'
+            onClick={() => init(100)}
+          >
+            100
+          </button>
+          <button
+            className='mx-2 bg-neutral-100 py-2 px-5 rounded-md'
+            onClick={() => init(numWords)}
+          >
+            Reset
+          </button>
+        </div>
+        <div className='w-1/3' />
       </div>
-      <div className='bg-neutral-900 p-6 text-2xl flex flex-wrap outline-none relative'>
+
+      {/* Test */}
+      <div className='p-6 text-2xl flex flex-wrap outline-none relative items-center'>
         <div
           className='absolute inset-1 z-10 flex justify-center items-center outline-none backdrop-blur-md focus:backdrop-blur-none transition ease-in-out duration-300 overflow-hidden'
           onKeyDown={handleKeyDown}
