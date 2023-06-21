b8262ff98cec4e9cfb61b2bde0b3e6f75a39893d
Finn Jacobs <finn.jacobs99@gmail.com>
2023-06-21 17:41:26 -0400

    created footer

--- react-app/src/components/Footer.jsx
+++ react-app/src/components/Footer.jsx
@@ -1,7 +1,47 @@
 import React from 'react';
+import { AiFillGithub, AiFillLinkedin, AiOutlineLink } from 'react-icons/ai';
+
+const spanStyle = `text-secondary dark:text-secondary-dark transition ease-in-out duration-300`;
+const linkStyle = `cursor-pointer text-secondary dark:text-secondary-dark hover:text-white dark:hover:text-white transition ease-in-out duration-300`;
 
 const Footer = () => {
-  return <div className='border w-full p-10'>Footer</div>;
+  return (
+    <div className='flex justify-between items-center w-full p-10'>
+      <span className={spanStyle}>Developed by Finn Jacobs</span>
+      <ul className='flex'>
+        <li className={`mr-5 ${linkStyle}`}>
+          <a
+            href='https://github.com/finnjacobs99'
+            target='_blank'
+            className='flex items-center justify-center'
+          >
+            Portfolio
+            <AiOutlineLink className='ml-1' />
+          </a>
+        </li>
+        <li className={`mr-5 ${linkStyle}`}>
+          <a
+            href='https://github.com/finnjacobs99'
+            target='_blank'
+            className='flex items-center justify-center'
+          >
+            Github
+            <AiFillGithub className='ml-1' />
+          </a>
+        </li>
+        <li className={linkStyle}>
+          <a
+            href='https://www.linkedin.com/in/finn-jacobs-5808831bb/'
+            target='_blank'
+            className='flex items-center justify-center'
+          >
+            LinkedIn
+            <AiFillLinkedin className='ml-1' />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Footer;
