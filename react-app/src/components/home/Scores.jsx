44e74be9293c3fa8fb217e56f3468108cb83413c
Finn Jacobs <finn.jacobs99@gmail.com>
2023-06-22 12:55:43 -0400

    added more styling/better formatting

--- react-app/src/components/home/Scores.jsx
+++ react-app/src/components/home/Scores.jsx
@@ -9,14 +9,31 @@ const Scores = ({ userScores }) => {
 
   return (
     <div className='flex flex-col justify-center items-center w-full p-10'>
-      <div className='flex flex-col'>
+      {/* Labels */}
+      <div className='flex text-2xl w-full text-center py-5'>
+        <label className='w-1/4'>Time</label>
+        <label className='w-1/4'>WPM</label>
+        <label className='w-1/4'>Accuracy</label>
+        <label className='w-1/4'>Correct</label>
+      </div>
+
+      {/* Divider */}
+      <div className='w-full bg-secondary dark:bg-secondary-dark h-[2px] rounded-full transition ease-in-out duration-300' />
+
+      {/* Scores */}
+      <div className='flex flex-col w-full py-5'>
         {scores.map((score, index) => {
+          const time = `${score.time.toFixed(2)}s`;
+          const wpm = score.wpm.toFixed(2);
+          const acc = `${score.accuracy.toFixed(2)}%`;
+          const correct = `${score.wordsCorrect}/${score.testLength}`;
           return (
-            <span key={index}>{`Time: ${score.time.toFixed(
-              2
-            )} WPM: ${score.wpm.toFixed(2)} Accuracy: ${score.accuracy.toFixed(
-              2
-            )} Correct: ${score.wordsCorrect}/${score.testLength}`}</span>
+            <div key={index} className='flex text-2xl text-center'>
+              <span className='w-1/4'>{time}</span>
+              <span className='w-1/4'>{wpm}</span>
+              <span className='w-1/4'>{acc}</span>
+              <span className='w-1/4'>{correct}</span>
+            </div>
           );
         })}
       </div>
