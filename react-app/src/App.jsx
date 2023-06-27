7f7a58b9c89fb22ece4e7b1fcc109d165b2c027f
Finn Jacobs <finn.jacobs99@gmail.com>
2023-06-27 17:05:02 -0400

    added context for user authentication

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -7,6 +7,7 @@ import {
   Footer,
 } from './components/index';
 import { Routes, Route } from 'react-router-dom';
+import { AuthContextProvider } from './contexts/AuthContext';
 
 const App = () => {
   const [darkMode, setDarkMode] = useState(
@@ -19,25 +20,27 @@ const App = () => {
   }
 
   return (
-    <div className={darkMode ? 'dark' : ''}>
-      <div className='flex flex-grow w-full min-h-screen bg-primary dark:bg-primary-dark text-white font-mono transition ease-in-out duration-300'>
-        {/* Content Container */}
-        <div className='w-full hidden sm:flex flex-col items-center justify-between 2xl:px-0 px-10 max-w-screen-2xl mx-auto'>
-          <Navbar onClickToggleDark={toggleDarkMode} />
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/leaderboards' element={<Leaderboards />} />
-            <Route path='/account' element={<Account />} />
-          </Routes>
-          <Footer />
-        </div>
+    <AuthContextProvider>
+      <div className={darkMode ? 'dark' : ''}>
+        <div className='flex flex-grow w-full min-h-screen bg-primary dark:bg-primary-dark text-white font-mono transition ease-in-out duration-300'>
+          {/* Content Container */}
+          <div className='w-full hidden sm:flex flex-col items-center justify-between 2xl:px-0 px-10 max-w-screen-2xl mx-auto'>
+            <Navbar onClickToggleDark={toggleDarkMode} />
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/leaderboards' element={<Leaderboards />} />
+              <Route path='/account' element={<Account />} />
+            </Routes>
+            <Footer />
+          </div>
 
-        {/* Warning for mobile users */}
-        <div className='flex sm:hidden h-screen w-full justify-center items-center px-10'>
-          <span>Sorry! This site does not yet support mobile devices.</span>
+          {/* Warning for mobile users */}
+          <div className='flex sm:hidden h-screen w-full justify-center items-center px-10'>
+            <span>Sorry! This site does not yet support mobile devices.</span>
+          </div>
         </div>
       </div>
-    </div>
+    </AuthContextProvider>
   );
 };
 
