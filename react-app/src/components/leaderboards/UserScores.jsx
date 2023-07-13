84a3ce165b22fc4c28bfd17f9f961d29fd332280
Finn Jacobs <finn.jacobs99@gmail.com>
2023-07-13 15:35:50 -0400

    added personal scoreboard functionality

--- react-app/src/components/leaderboards/UserScores.jsx
+++ react-app/src/components/leaderboards/UserScores.jsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { animate } from '../../styles';
+// import { UserAuth } from '../../contexts/AuthContext';
+// import { db } from '../../firebase';
+// import { collection, query, orderBy, limit, getDocs } from 'firebase/firestore';
+import { useTestResults } from '../../contexts/TestResultsContext';
 
 const UserScores = () => {
+  const testResults = useTestResults();
+
+  // const [testResults, setTestResults] = useState([]);
+  // const { user } = UserAuth();
+
+  // useEffect(() => {
+  //   const fetchTestResults = async () => {
+  //     if (!user) return;
+
+  //     const q = query(
+  //       collection(db, 'users', user.uid, 'testResults'),
+  //       orderBy('timestamp', 'desc'),
+  //       limit(10)
+  //     );
+  //     const querySnapshot = await getDocs(q);
+
+  //     const results = [];
+  //     querySnapshot.forEach((doc) => {
+  //       results.push(doc.data());
+  //     });
+
+  //     setTestResults(results);
+  //   };
+
+  //   fetchTestResults();
+  // }, [user]);
+
+  // useEffect(() => {
+  //   console.log(testResults);
+  // }, [testResults]);
+
   return (
-    <div className='flex flex-col justify-center items-center w-full p-10 border'>
+    <div className='flex flex-col justify-center items-center w-full p-10'>
       {/* Labels */}
       <div className='flex text-2xl w-full text-center'>
         <span className='w-1/4'>Time</span>
@@ -18,12 +53,14 @@ const UserScores = () => {
       />
 
       {/* Scores */}
-      <div className='flex text-2xl text-center w-full'>
-        <span className='w-1/4'>Time</span>
-        <span className='w-1/4'>wpm</span>
-        <span className='w-1/4'>accuracy</span>
-        <span className='w-1/4'>correct</span>
-      </div>
+      {testResults.map((result, idx) => (
+        <div key={idx} className='flex text-2xl text-center w-full mb-5'>
+          <span className='w-1/4'>{`${result.time.toFixed(2)}s`}</span>
+          <span className='w-1/4'>{result.wpm.toFixed(2)}</span>
+          <span className='w-1/4'>{`${result.accuracy.toFixed(2)} %`}</span>
+          <span className='w-1/4'>{`${result.wordsCorrect}/${result.testLength}`}</span>
+        </div>
+      ))}
     </div>
   );
 };
