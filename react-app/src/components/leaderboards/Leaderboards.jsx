b8916e3372a8a854b60e9f37f3a28ab9de2d91b1
Finn Jacobs <finn.jacobs99@gmail.com>
2023-07-13 15:35:15 -0400

    minor styling

--- react-app/src/components/leaderboards/Leaderboards.jsx
+++ react-app/src/components/leaderboards/Leaderboards.jsx
@@ -10,8 +10,9 @@ const Leaderboards = () => {
 
   return (
     <div className='flex flex-col justify-center items-center w-full'>
+      <h1 className='text-2xl'>Your Test Results:</h1>
       {user ? <UserScores /> : <div>Sign in to track your scores</div>}
-      <div>Leaderboard</div>
+      {/* <div>Leaderboard</div> */}
     </div>
   );
 };
