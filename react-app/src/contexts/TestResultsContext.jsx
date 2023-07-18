9f0981f81e5a960e1176f1c084c84493b745f835
Finn Jacobs <finn.jacobs99@gmail.com>
2023-07-18 15:01:24 -0400

    added functions to fetch personal best and leaderboard

--- react-app/src/contexts/TestResultsContext.jsx
+++ react-app/src/contexts/TestResultsContext.jsx
@@ -1,12 +1,22 @@
 import React, { useEffect, useState, createContext, useContext } from 'react';
 import { UserAuth } from './AuthContext';
 import { db } from '../firebase';
-import { collection, query, orderBy, limit, getDocs } from 'firebase/firestore';
+import {
+  collection,
+  query,
+  orderBy,
+  limit,
+  getDocs,
+  doc,
+  getDoc,
+} from 'firebase/firestore';
 
 const TestResultsContext = createContext([]);
 
 const TestResultsProvider = ({ children }) => {
   const [testResults, setTestResults] = useState([]);
+  const [personalBest, setPersonalBest] = useState({});
+  const [leaderboard, setLeaderboard] = useState([]);
   const { user } = UserAuth();
 
   useEffect(() => {
@@ -27,11 +37,44 @@ const TestResultsProvider = ({ children }) => {
       setTestResults(results);
     };
 
+    const fetchPersonalBest = async () => {
+      if (!user) return;
+
+      const docRef = doc(db, 'users', user.uid);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.data().bestWpm) {
+        const pb = {
+          time: docSnap.data().bestTime,
+          wpm: docSnap.data().bestWpm,
+          accuracy: docSnap.data().bestAccuracy,
+          wordsCorrect: docSnap.data().bestWordsCorrect,
+          testLength: docSnap.data().bestTestLength,
+        };
+        setPersonalBest(pb);
+      }
+    };
+
+    const fetchLeaderboard = async () => {
+      const q = query(collection(db, 'users'), orderBy('bestWpm', 'desc'));
+      const querySnapshot = await getDocs(q);
+
+      const results = [];
+      querySnapshot.forEach((doc) => {
+        results.push(doc.data());
+      });
+      setLeaderboard(results);
+    };
+
     fetchTestResults();
+    fetchPersonalBest();
+    fetchLeaderboard();
   }, [user]);
 
   return (
-    <TestResultsContext.Provider value={testResults}>
+    <TestResultsContext.Provider
+      value={{ testResults, personalBest, leaderboard }}
+    >
       {children}
     </TestResultsContext.Provider>
   );
