6bbb70a0a8fbf82fcc8cea4189da00ccef859271
Finn Jacobs <finn.jacobs99@gmail.com>
2023-07-26 09:20:15 -0400

    cleanup

--- react-app/src/contexts/AuthContext.jsx
+++ react-app/src/contexts/AuthContext.jsx
@@ -60,7 +60,6 @@ export const AuthContextProvider = ({ children }) => {
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      console.log(currentUser);
       setUser(currentUser);
     });
     return () => {
